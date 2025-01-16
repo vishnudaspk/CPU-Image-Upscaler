647075e46649d13460929c1216c5d445b1449523
vishnudaspk <vishnu71y13@gmail.com>
2025-01-16 18:54:50 +0530

    Remove pyvenv.cfg from the repository

--- gen/Lib/site-packages/gradio/templates/frontend/assets/index-42d38290.js
+++
@@ -1,2 +0,0 @@
-import{S as a}from"./StaticForm-23a48556.js";import"./index-2519a27e.js";export{a as default};
-//# sourceMappingURL=index-42d38290.js.map
--- gen/Lib/site-packages/gradio/templates/frontend/assets/index-81a4ec15.js
+++
@@ -1,2 +0,0 @@
-import{S as a}from"./StaticColumn-6bd0cb0d.js";import"./index-2519a27e.js";export{a as default};
-//# sourceMappingURL=index-81a4ec15.js.map
